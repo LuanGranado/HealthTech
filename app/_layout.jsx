f5951b549f0712a26edabf2cabd997cbab1ae132
LuanGranado <131273881+LuanGranado@users.noreply.github.com>
2024-09-06 09:07:17 -0300

    update _layout.jsx
    mudança do header

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -4,6 +4,10 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="Cadastro1" options={{ headerShown: false }} />
+      <Stack.Screen name="Cadastro2" options={{ headerShown: false }} />
+      <Stack.Screen name="Cadastro3" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
